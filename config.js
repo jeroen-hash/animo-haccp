53a5b4646bd3e47151f5dad52ee292fa94053942
jeroen-hash <jeroen@freelance-animo.com>
2026-09-24 14:15:31 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-// Vul hier de waarden uit Supabase > Project Settings > API in.
-// Gebruik uitsluitend de Project URL en de anon/publishable key.
-// Plaats NOOIT de service_role key in browsercode.
-window.ANIMO_CONFIG = {
-  SUPABASE_URL: "https://xwjcwravfeldndwlugai.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_nMuGFmDa34Xm1HQ21kTK9A_uZIRtapI"
-};
+window.ANIMO_CONFIG={SUPABASE_URL:"https://xwjcwravfeldndwlugai.supabase.co",SUPABASE_ANON_KEY:"sb_publishable_nMuGFmDa34Xm1HQ21kTK9A_uZIRtapI"};
